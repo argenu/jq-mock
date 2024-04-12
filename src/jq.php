85cddefe8b254904f60977fabb64d520ff731e52
Avchin Aleksandr <aavchin@gmail.com>
2024-04-12 12:01:44 +0300

    init

---
+++ src/jq.php
@@ -0,0 +1,23 @@
+<?php
+
+class JQMock {
+
+    public function __construct()
+    {  
+    }
+
+    public static function execForPath($path, $query) {
+
+        $command = "bash -c \"cat $path | jq $query\"";
+    
+        $output = [];
+    
+        exec($command, $output);
+    
+        return $output;
+    
+    }
+
+}
+
+
